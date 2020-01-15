2c40d1d8a78caefceb235af763aca3e984238bb2
aal-brown <adlai.brown@gmail.com>
2020-01-15 20:38:40 +0000

    Added a new module to store the constants used in the game gile, IP and PORT.

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require('net'); //Require the right module
+const constant = require('constants'); //Require the right module
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
+    host: constant.IP,
+    port: constant.PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
