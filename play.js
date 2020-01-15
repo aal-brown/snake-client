7432356375253048f3abfcbc930abc982482e654
aal-brown <adlai.brown@gmail.com>
2020-01-15 19:25:52 +0000

    Set up user input, and an escape function.

--- play.js
+++ play.js
@@ -1,10 +1,30 @@
 const net = require("net");
 const { connect } = require("./client.js");
 
-
 //Establish connection with game server
-
-
 
 console.log('Connecting ...');
 connect();
+
+
+const handleUserInput = function(data) {
+  if (data === "\u0003") {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+    
+  stdin.on("data", (data) => {handleUserInput(data)});
+
+  return stdin;
+};
+
+
+
+
+setupInput();
